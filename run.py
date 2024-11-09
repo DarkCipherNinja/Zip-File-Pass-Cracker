2b09852df480e694c78595a067078f4b1bd664cc
Black_Ninga <132413669+DarkCipherNinja@users.noreply.github.com>
2024-11-09 07:23:41 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
+import sys
+sys.path.append("build/lib.linux-aarch64-cpython-312")  # Add the path to your .so file
+import bruteforce
+
+# Now you can call the functions from the bruteforce module
+bruteforce.main()
 
